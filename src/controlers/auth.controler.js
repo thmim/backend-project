2a541e95f5c37d03353a2d54eb890ac80791819c
thmim <mdtaharimmim1010@gmail.com>
2026-03-09 14:44:06 +0600

    create login api

--- src/controlers/auth.controler.js
+++ src/controlers/auth.controler.js
@@ -42,4 +42,46 @@ async function registerUser(req, res) {
     })
 }
 
-module.exports = {registerUser};
+// login function
+async function loginUser(req,res) {
+    const {username,email,password} = req.body;
+    const user = await userModel.findOne({
+        $or:[
+            {username},
+            {email}
+        ]
+    })
+    if(!user){
+        res.status(401).send({
+            message:"Invalid credential"
+        })
+    }
+
+    const isPasswordValid = await bcrypt.compare(password,user.password)
+    if(!isPasswordValid){
+        res.status(401).send({
+            message:"Invalid credential"
+        })
+    }
+
+    const token = jwt.sign({
+        id:user._id,
+        role:user.role
+    },process.env.JWT_SECRET_KEY)
+
+    res.cookie("token",token);
+
+    res.status(200).send({
+        message:"Login sucessfully",
+        user: {
+            username: user.username,
+            email: user.email,
+            password: user.password,
+            id: user._id,
+            role: user.role,
+        }
+
+    })
+}
+
+module.exports = {registerUser,loginUser};
--- src/routes/auth.routes.js
+++ src/routes/auth.routes.js
@@ -1,7 +1,10 @@
 const express = require("express");
 const authControler = require("../controlers/auth.controler")
 const router = express.Router();
-
+// register api
 router.post('/register',authControler.registerUser)
+
+// login api
+router.post('/login',authControler.loginUser)
 
 module.exports = router;
